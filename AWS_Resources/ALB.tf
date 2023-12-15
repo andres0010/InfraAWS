867d94abb149ffdab9ce9fae4d3c3255cfd8d29e
harlen-delvalle <harlen.delvalle@gmail.com>
2023-12-14 20:12:27 -0500

    agrego archivo ALB configuracion de balanciador de carga

--- AWS_Resources/ALB.tf
+++ AWS_Resources/ALB.tf
@@ -2,3 +2,25 @@ provider "aws" {
   region = var.region
 }
 
+resource "aws_lb" "my_lb" {
+  name               = "my-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_security_group.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+resource "aws_lb_target_group" "my_target_group" {
+  name     = "my-target-group"
+  port     = var.container_port
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my_vpc.id
+  target_type = "ip"  # Ajuste importante para Fargate con awsvpc
+
+  health_check {
+    path     = "/health"  # Ajusta según las necesidades de tu aplicación
+    port     = var.container_port
+    protocol = "HTTP"
+  }
+}
+
