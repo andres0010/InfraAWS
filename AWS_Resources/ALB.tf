36b0aeeeda553446be31b07b83de89e3330b0225
Jonny Suarez <jonnyandres0010@hotmail.com>
2023-12-15 07:18:01 -0500

    Modificaciones en archivo terraform y ECS

--- AWS_Resources/ALB.tf
+++ AWS_Resources/ALB.tf
@@ -7,18 +7,18 @@ resource "aws_lb" "my_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.ecs_security_group.id]
-  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+  subnets            = [aws_subnet.subnet_publica_1.id, aws_subnet.subnet_publica_2.id]
 }
 
 resource "aws_lb_target_group" "my_target_group" {
   name     = "my-target-group"
   port     = var.container_port
   protocol = "HTTP"
-  vpc_id   = aws_vpc.my_vpc.id
+  vpc_id   = aws_vpc.vpcGeneral.id
   target_type = "ip"  # Ajuste importante para Fargate con awsvpc
 
   health_check {
-    path     = "/health"  # Ajusta según las necesidades de tu aplicación
+    path     = "/health"
     port     = var.container_port
     protocol = "HTTP"
   }
